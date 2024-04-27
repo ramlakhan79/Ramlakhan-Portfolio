87f70ae3dabe1dd802462ed3767f2625ee5bf0f9
Ramlakhan Lodhi <ramlakhanlodhi229@gmail.com>
2024-04-27 14:34:44 +0530

    add resume

--- src/constants/data.js
+++ src/constants/data.js
@@ -20,53 +20,33 @@ export const navMenu = [
 ]
 
 export const projects = [
-  {
-    title: "My Personal Portfolio",
-    src: "/assets/images/projects/portfolio.png",
-    altText: "Screenshot of Portfolio home page",
-    ariaLabel:
-      "Opens modal window with more information about the My Personal Portfolio web project",
-    details: {
-      description:
-        "Welcome to Ramlakhan, where innovation meets expertise. Based in the web technologies, our portfolio showcases a diverse range of web development projects and services, each reflecting our commitment to quality, creativity, and client satisfaction. Whether you're looking for a sleek business website, a dynamic e-commerce platform, or a custom web application, our portfolio has something for you.",
-      technologies: [
-        {
-          title: "HTML",
-          Icon: Icons.HtmlIcon,
-        },
-        {
-          title: "CSS",
-          Icon: Icons.CSSIcon,
-        },
-        {
-          title: "JavaScript",
-          Icon: Icons.JavascriptIcon,
-        },
-        // {
-        //   title: "WordPress",
-        //   Icon: Icons.WordPressIcon,
-        // },
-        // {
-        //   title: "PHP",
-        //   Icon: Icons.PHPIcon,
-        // },
-        // {
-        //   title: "BackEnd",
-        //   Icon: Icons.DatabaseIcon,
-        // },
-        // {
-        //   title: "jQuery",
-        //   Icon: Icons.JQueryIcon,
-        // },
-        // {
-        //   title: "API",
-        //   Icon: Icons.APIIcon,
-        // },
-      ],
-      url: "https://ramlakhan130.netlify.app/",
-      githubUrl: null,
-    },
-  },
+  // {
+  //   title: "My Personal Portfolio",
+  //   src: "/assets/images/projects/portfolio.png",
+  //   altText: "Screenshot of Portfolio home page",
+  //   ariaLabel:
+  //     "Opens modal window with more information about the My Personal Portfolio web project",
+  //   details: {
+  //     description:
+  //       "Welcome to Ramlakhan, where innovation meets expertise. Based in the web technologies, our portfolio showcases a diverse range of web development projects and services, each reflecting our commitment to quality, creativity, and client satisfaction. Whether you're looking for a sleek business website, a dynamic e-commerce platform, or a custom web application, our portfolio has something for you.",
+  //     technologies: [
+  //       {
+  //         title: "HTML",
+  //         Icon: Icons.HtmlIcon,
+  //       },
+  //       {
+  //         title: "CSS",
+  //         Icon: Icons.CSSIcon,
+  //       },
+  //       {
+  //         title: "JavaScript",
+  //         Icon: Icons.JavascriptIcon,
+  //       },
+  //     ],
+  //     url: "https://ramlakhan130.netlify.app/",
+  //     githubUrl: null,
+  //   },
+  // },
 
   {
     title: "Chat Web Application",
@@ -78,18 +58,6 @@ export const projects = [
       description:
         "A chat web application enables real-time communication between users through text-based messages. It typically includes features like message history, user authentication, notifications, and group chats, providing a seamless and interactive platform for conversation and collaboration.",
       technologies: [
-        // {
-        //   title: "HTML",
-        //   Icon: Icons.HtmlIcon,
-        // },
-        // {
-        //   title: "CSS",
-        //   Icon: Icons.CSSIcon,
-        // },
-        // {
-        //   title: "JavaScript",
-        //   Icon: Icons.JavascriptIcon,
-        // },
         {
           title: "MongoDB",
           Icon: Icons.MongoDBIcon,
@@ -108,7 +76,7 @@ export const projects = [
         },
       ],
       url: "https://chitchat-165i.onrender.com/",
-      githubUrl: null,
+      githubUrl: "https://github.com/ramlakhan79/Chat-Web-Application",
     },
   },
   {
@@ -121,18 +89,6 @@ export const projects = [
       description:
         "A quiz web application allows users to take interactive quizzes on various topics. It typically offers multiple-choice questions, real-time scoring, and results analysis. Features may include timed quizzes, leaderboards, personalized question sets, and social sharing, creating an engaging and educational experience for users.",
       technologies: [
-        // {
-        //   title: "HTML",
-        //   Icon: Icons.HtmlIcon,
-        // },
-        // {
-        //   title: "CSS",
-        //   Icon: Icons.CSSIcon,
-        // },
-        // {
-        //   title: "JavaScript",
-        //   Icon: Icons.JavascriptIcon,
-        // },
         {
           title: "MongoDB",
           Icon: Icons.MongoDBIcon,
@@ -149,21 +105,9 @@ export const projects = [
           title: "NodeJS",
           Icon: Icons.NodeJSIcon,
         },
-        // {
-        //   title: "WordPress",
-        //   Icon: Icons.WordPressIcon,
-        // },
-        // {
-        //   title: "PHP",
-        //   Icon: Icons.PHPIcon,
-        // },
-        // {
-        //   title: "BackEnd",
-        //   Icon: Icons.DatabaseIcon,
-        // },
       ],
-      url: "https://www.threeseeds.org/",
-      githubUrl: null,
+      url:null,
+      githubUrl: "https://github.com/ramlakhan79/Quiz-Web-App",
     },
   },
 ];
@@ -171,10 +115,10 @@ export const projects = [
 export const testimonials = [
     {
         quote: "I am incredibly grateful for Ramlakhan remarkable efforts of completely revamping our nonprofit's website. His dedication and expertise in enhancing the site's security measures while also ensuring a seamless user experience has been nothing short of exceptional. The new website not only boasts a robust security infrastructure that safeguards sensitive data, but it also welcomes visitors with an intuitive and user-friendly interface. Ramlakhan took our organization's mission to heart and translated it into an online platform that not only represents our values but also engages our audience effectively. His selfless commitment to improving our online presence has undoubtedly elevated our nonprofit's reach and impact. We owe him a debt of gratitude for his invaluable contribution in making our website a secure and inviting space for all.",
-        name: 'Pankaj P.',
+        name: 'Lakhan L.',
         title: 'Executive Director of Three Seeds Organization',
-        src: '/assets/images/testimonials/alicia.png',
-        altText: 'Profile of Pankaj P.',
+        src: '/assets/images/testimonials/testimo.png',
+        altText: 'Profile of Lakhan L.',
     }
 ]
 
@@ -222,10 +166,6 @@ export const techSkills = [
         title: "Java",
         Icon: Icons.JavaIcon,
       },
-      // {
-      //     title: 'VBA',
-      //     Icon: Icons.VBIcon,
-      // },
     ],
   },
 
@@ -236,10 +176,6 @@ export const techSkills = [
         title: "WordPress",
         Icon: Icons.WordPressIcon,
       },
-      // {
-      //     title: 'Wix',
-      //     Icon: Icons.WixIcon,
-      // },
     ],
   },
   {
@@ -257,10 +193,6 @@ export const techSkills = [
         title: "Tailwind CSS",
         Icon: Icons.TailwindCSSIcon,
       },
-      // {
-      //     title: 'SASS, SCSS',
-      //     Icon: Icons.SassIcon,
-      // },
       {
         title: "JavaScript",
         Icon: Icons.JavascriptIcon,
@@ -269,19 +201,11 @@ export const techSkills = [
         title: "React",
         Icon: Icons.ReactIcon,
       },
-      // {
-      //     title: 'jQuery',
-      //     Icon: Icons.JQueryIcon,
-      // },
     ],
   },
   {
     category: "Back-End Development",
     skills: [
-      // {
-      //     title: 'PHP',
-      //     Icon: Icons.PHPIcon,
-      // },
       {
         title: "MongoDB",
         Icon: Icons.MongoDBIcon,
@@ -298,10 +222,6 @@ export const techSkills = [
         title: "SQL Server",
         Icon: Icons.SqlServerIcon,
       },
-      // {
-      //     title: 'SSRS',
-      //     Icon: Icons.SsrsIcon,
-      // },
     ],
   },
 
@@ -342,14 +262,6 @@ export const techSkills = [
   {
     category: "Microsoft Products",
     skills: [
-      // {
-      //     title: 'Sharepoint',
-      //     Icon: Icons.SharepointIcon,
-      // },
-      // {
-      //     title: 'Excel',
-      //     Icon: Icons.ExcelIcon,
-      // },
       {
         title: "Access",
         Icon: Icons.AccessIcon,
@@ -367,10 +279,6 @@ export const techSkills = [
         title: "REST APIs",
         Icon: Icons.RestAPIIcon,
       },
-      // {
-      //     title: 'LocalWP',
-      //     Icon: Icons.LocalWP,
-      // },
     ],
   },
 ];
@@ -384,10 +292,6 @@ export const softSkills = [
         title: 'Teamwork',
         Icon: Icons.TeamworkIcon,
     },
-    // {
-    //     title: 'Public Speaking',
-    //     Icon: Icons.PublicSpeakingIcon,
-    // },
     {
         title: 'Time Management',
         Icon: Icons.ScheduleIcon,
@@ -412,68 +316,9 @@ export const workExp = [
             "Demonstrate expertise in designing custom WordPress themes and plugins in PHP, enhancing website functionality and user interactivity",
         ],
     },
-    // {
-    //     company: '100Devs',
-    //     location: 'Remote',
-    //     positions: [
-    //         {
-    //             title: 'Full-Stack Software Engineer',
-    //             startDate: new Date(2022, 3),
-    //             endDate: new Date(2023, 0),
-    //         },
-    //     ],
-    //     tasks: [
-    //         "Designed semantically structured and responsive websites for mobile and desktop devices",
-    //         "Collaborated with a team of developers to construct full-stack web applications",
-    //         "Utilized agile methodologies such as SCRUM for project management",
-    //     ],
-    // },
-    // {
-    //     company: 'Xerox Corporation',
-    //     location: 'Webster, NY',
-    //     positions: [
-    //         {
-    //             title: 'Global Equipment Systems Engineer',
-    //             startDate: new Date(2015, 9),
-    //             endDate: new Date(2022, 2),
-    //         },
-    //     ],
-    //     tasks: [
-    //         "Developed device customization processes for Fortune 500 clients and government accounts",
-    //         "Ranked number one in my department on process quality and defects testing by standardizing automation and fail-safes",
-    //         "Provided technical support for remote production sites to facilitate the completion of device configurations",
-    //         "Collaborated with a small team to improve processes",
-    //         "Designed C# and .NET desktop application solutions to support engineers",
-    //         "Successfully completed project of revamping Extract-Transform-Load system that led to 95% improved efficiency in terms of execution time, and incorporated troubleshooting features",
-    //         "Designed and managed secure SharePoint websites for clients",
-    //         "Wrote ad-hoc SQL queries for statistical analysis",
-    //         "Wrote macros in Excel to populate customer data on printer labels",
-    //     ],
-    // },
 ];
 
 export const extracurriculars = [
-    // {
-    //     company: 'Toastmasters International Organization',
-    //     location: 'Pittsford, NY',
-    //     positions: [
-    //         {
-    //             title: 'Vice President of Public Relations',
-    //             startDate: new Date(2023, 6),
-    //             endDate: 'present',
-    //         },
-    //         {
-    //             title: 'Sergeant at Arms',
-    //             startDate: new Date(2022, 4),
-    //             endDate: new Date(2023, 6),
-    //         },
-    //     ],
-    //     tasks: [
-    //         "Develop strategies to promote the club to the community, potential members, and guests",
-    //         "Manage the club's social media accounts and website to share club events, achievements, and information",
-    //         "Create content that showcases the club's activities and benefits",
-    //     ],
-    // },
     {
         company: 'Literacy Bhopal',
         location: 'East Bhopal, India',
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,9 +8,9 @@ export default function About() {
             <h1 className="text-center max-w-[18em]">About Ramlakhan</h1>
             <img className="w-[300px] profile-border" src="/assets/images/ramlakhan1.png" alt="Profile of Ramlakhan" />
             <p className="text-justify">Hello! My name is Ramlakhan, and I currently living in Bhopal , India..</p>
-            <p className="text-justify">I hold a BTech in Computer Engineering from the Sagar Institute of Science and Technology Bhopal. Additionally, I successfully managed a Fontend data in a database. My interest Competitive Programming and Data Structure and Algorithm.</p>
-            <p className="text-justify">Beyond coding and interacting with clients, I find joy in giving back to the community. Volunteering at the East Rochester Library allows me to troubleshoot technical issues and teach computer skills to eager learners. In my spare time, I unleash my creativity in the kitchen by baking a wide array of scrumptious desserts, which I take pleasure in sharing with friends and fellow Toastmasters.</p>
-            <p className="text-center">Here are a few of my baking creations:</p>
+            <p className="text-justify">I hold a BTech in Computer Engineering from the Sagar Institute of Science and Technology Bhopal. Additionally, I successfully managed a Frontend data in a database. My interest Competitive Programming and Data Structure and Algorithm.</p>
+            <p className="text-justify">Beyond coding and interacting with clients, I find joy in giving back to the community. Volunteering at the East Area Library allows me to troubleshoot technical issues and teach computer skills to eager learners. In my spare time, I take a picture of a moments</p>
+            <p className="text-center">Here are a few of my moments creations picturs:</p>
             <CubeCarousel name="desserts" carouselImages={dessertImages} />
             <p className="text-justify">My journey in web development is driven by the desire to make a meaningful impact on businesses and individuals alike. Combining my technical expertise, creativity, and dedication to continuous improvement, I strive to create compelling digital experiences that exceed expectations.</p>
         </section>
