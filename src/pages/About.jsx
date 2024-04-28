6c7a20ca1fa429b90641b6d2a3e34d4d1d7056d0
Ramlakhan Lodhi <ramlakhanlodhi229@gmail.com>
2024-04-28 23:34:43 +0530

    Correction Home Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     return (
         <section className="w-[100%] mt-9 sm:mt-10 md:mt-11 flex flex-col flex-nowrap items-center justify-center gap-7 px-mobileBound sm:px-8 sm:gap-8 md:gap-9 lg:gap-10">
             <h1 className="text-center max-w-[18em]">About Ramlakhan</h1>
-            <img className="w-[300px] profile-border" src="/assets/images/ramlakhan1.png" alt="Profile of Ramlakhan" />
+            <img className="w-[300px] profile-border " src="/assets/images/ramlakhan1.png" alt="Profile of Ramlakhan" />
             <p className="text-justify">Hello! My name is Ramlakhan, and I currently living in Bhopal , India..</p>
             <p className="text-justify">I hold a BTech in Computer Engineering from the Sagar Institute of Science and Technology Bhopal. Additionally, I successfully managed a Frontend data in a database. My interest Competitive Programming and Data Structure and Algorithm.</p>
             <p className="text-justify">Beyond coding and interacting with clients, I find joy in giving back to the community. Volunteering at the East Area Library allows me to troubleshoot technical issues and teach computer skills to eager learners. In my spare time, I take a picture of a moments</p>
