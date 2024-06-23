a73f1cb2ef34f1cd88e085b880b8d34f92dc3c96
Ramlakhan Lodhi <ramlakhanlodhi229@gmail.com>
2024-06-23 15:23:08 +0530

    Coding Part Color Updated

--- src/pages/Coding.jsx
+++ src/pages/Coding.jsx
@@ -1,6 +1,7 @@
 
 // import React from 'react';
 import React, { useState, useEffect } from 'react';
+import { useOutletContext } from "react-router-dom";
 // import Box from '@mui/joy/Box';
 // import CircularProgress from '@mui/joy/CircularProgress';
 // import ReportIcon from '@mui/icons-material/Report';
@@ -8,6 +9,7 @@ import React, { useState, useEffect } from 'react';
 
 
 export default function Coding() {
+    const [isDarkMode] = useOutletContext();
 
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -36,65 +38,55 @@ export default function Coding() {
 
     return (
         <section className="w-[100%] mt-9 sm:mt-10 md:mt-11 flex flex-col flex-nowrap items-center justify-center gap-7 px-mobileBound sm:px-8 sm:gap-8 md:gap-9 lg:gap-10">
-            {/* <Box sx={{ display: 'flex', gap: 2, alignItems: 'center', flexWrap: 'wrap' }}>
-                <CircularProgress color="warning">
-                    <WarningIcon color="warning" />
-                </CircularProgress>
-                <CircularProgress size="lg" determinate value={66.67}>
-                    2 / 3
-                </CircularProgress>
-                <CircularProgress color="danger" sx={{ '--CircularProgress-size': '80px' }}>
-                    <ReportIcon color="danger" />
-                </CircularProgress>
-            </Box> */}
             <div className="bg-gray-900 p-4 rounded-md shadow-md">
                 <div className="flex justify-between items-center">
                     <div>
                         <div className="flex items-center">
                             <svg
-                                viewBox="0 0 100 100"
+                                viewBox="0 0 150 150"
                                 className="w-34 h-34"
+                                // style={{ transform: 'scale(1.5)' }} 
                             >
                                 <circle
-                                    cx="50"
-                                    cy="50"
-                                    r="45"
+                                    cx="75"
+                                    cy="75"
+                                    r="65"
                                     stroke="#F59E0B"
                                     strokeWidth="8"
                                     fill="transparent"
                                 />
                                 <circle
-                                    cx="50"
-                                    cy="50"
-                                    r="45"
+                                    cx="75"
+                                    cy="75"
+                                    r="65"
                                     stroke="#38B2AC"
                                     strokeWidth="8"
                                     fill="transparent"
-                                    strokeDasharray="282.7433388230814 282.7433388230814"
+                                    strokeDasharray="232.7433388230814 282.7433388230814"
                                     strokeDashoffset="204.16385182057517"
                                 />
                                 <circle
-                                    cx="50"
-                                    cy="50"
-                                    r="45"
+                                    cx="75"
+                                    cy="75"
+                                    r="65"
                                     stroke="#E53E3E"
                                     strokeWidth="8"
                                     fill="transparent"
-                                    strokeDasharray="282.7433388230814 282.7433388230814"
+                                    strokeDasharray="252.7433388230814 282.7433388230814"
                                     strokeDashoffset="78.5794869925063"
                                 />
                                 <text
-                                    x="50"
-                                    y="50"
+                                    x="75"
+                                    y="75"
                                     textAnchor="middle"
                                     fontSize="12"
-                                    fill="#fff"
+                                    fill={isDarkMode ? '#fff' : '#000'}
                                 >
                                     {data.totalSolved}/{data.totalQuestions}
                                 </text>
                                 <text
-                                    x="50"
-                                    y="75"
+                                    x="75"
+                                    y="95"
                                     textAnchor="middle"
                                     fontSize="12"
                                     fill="#38B2AC"
@@ -108,7 +100,7 @@ export default function Coding() {
                     <div className="ml-8">
                         <div className="bg-gray-800 p-2 rounded-md mb-2">
                             <div className="flex items-center">
-                                <span className="text-sm font-medium text-cyan-400 mr-2">Easy</span>
+                                <span className="text-sm font-medium text-blue-400 mr-2">Easy</span>
                                 <span className="text-sm font-medium text-gray-400">{data.easySolved}/{data.totalEasy}</span>
                             </div>
                         </div>
